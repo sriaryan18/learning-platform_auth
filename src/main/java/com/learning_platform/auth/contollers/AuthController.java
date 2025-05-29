15c17363a426847e311cad3457a0eb2e55c4c064
Aryan Shrivastava <aryan.shrivastava@kuliza.com>
2025-05-30 02:29:57 +0530

    cookies sent in response

--- src/main/java/com/learning_platform/auth/contollers/AuthController.java
+++ src/main/java/com/learning_platform/auth/contollers/AuthController.java
@@ -1,29 +1,43 @@
 package com.learning_platform.auth.contollers;
 
-
 import com.learning_platform.auth.dtos.LoginDto;
 import com.learning_platform.auth.dtos.LoginResponseDto;
 import com.learning_platform.auth.dtos.SignUpDto;
 import com.learning_platform.auth.service.AuthService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/auth/api/v1")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:3000")
 public class AuthController {
 
     @Autowired
     AuthService authService;
 
+    @Value("${jwt.access-expiration}")
+    private int accessExpiration;
+
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
 
         try {
             LoginResponseDto loginResponseDto = authService.handleLogin(loginDto);
+
+            Cookie cookie = new Cookie("token", loginResponseDto.getAccessToken());
+            cookie.setHttpOnly(true);
+            cookie.setSecure(false);
+            cookie.setMaxAge(accessExpiration);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+
             return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
         } catch (Exception e) {
             throw new RuntimeException(e);
