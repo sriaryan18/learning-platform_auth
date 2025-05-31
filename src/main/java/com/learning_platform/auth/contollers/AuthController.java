5ebcc77e4c7a9e859a2c1446cfa4739f2185d080
Aryan Shrivastava <aryan.shrivastava@kuliza.com>
2025-05-31 20:07:45 +0530

    removed cors annotation

--- src/main/java/com/learning_platform/auth/contollers/AuthController.java
+++ src/main/java/com/learning_platform/auth/contollers/AuthController.java
@@ -10,13 +10,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie.SameSite;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth/api/v1")
-@CrossOrigin("http://localhost:3000")
 public class AuthController {
 
     @Autowired
@@ -43,6 +44,11 @@ public class AuthController {
             throw new RuntimeException(e);
         }
 
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("TEST");
     }
 
     @PostMapping("/signup")
